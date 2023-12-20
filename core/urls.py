42d8dc269c3335d979a84ef3cb5aeccfaebb007d
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-12-20 14:16:17 -0300

    :sparkles: feat: Novo app para analilse de fundos imobiliarios

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('horus.urls'))
+    path('', include('horus.urls')),
+    path('analytics/',include('analytics.urls'))
 ]
