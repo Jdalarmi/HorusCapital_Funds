c2b9f533058aeef17b07ccbbb8512adeb4b4969e
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-12-21 14:53:37 -0300

    :sparkles: feat: Inicio de receber os dados na view para tratar na analise views function

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class JurosTable(models.Model):
+    value_start = models.FloatField()
+    value_month = models.FloatField()
+    value_rentability = models.FloatField()
+    
--- analytics/views.py
+++ analytics/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import JurosTable
+
+list_month = ['Janeiro', 'Fevereiro']
+
+
+def future_values(request):
+    if request.method == 'POST':
+        value_start = float(request.POST.get('value_start').replace(",", "."))
+        value_by = float(request.POST.get('value_by').replace(",", "."))
+        value_rentability = float(request.POST.get('value_rentability').replace(",", "."))
+        
+
+    return render(request, 'analytics/analise_copy.html')
 
 def analise(request):
     return render(request, 'analytics/analise.html')
-
-def future_values(request):
-    return render(request, 'analytics/analise_copy.html')
