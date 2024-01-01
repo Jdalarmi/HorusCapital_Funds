718d629f84f93878a2954ccdc64f0b284465fee1
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-01 10:38:59 -0300

    :sparkles: feat: Pegando os valores para criar tabela com fiis

--- scraping/views.py
+++ scraping/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def start(request):
+    if request.method == 'POST':
+        fundo = request.POST.get('fundo')
     return render(request, 'scraping/start.html')
