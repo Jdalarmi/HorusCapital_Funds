24f3368caacbd145cee9ea220e7e7d5cd2652c85
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-16 09:31:09 -0300

    :sparkles: feat: Ajuste na view que recebe fii como parametro antes de salvar no BD

--- scraping/views.py
+++ scraping/views.py
@@ -6,7 +6,4 @@ def start(request):
     print(colect_fund('VRTA11'))
     if request.method == 'POST':
         fundo = request.POST.get('fundo')
-        # dados_fund = FundsHorus.objects.get_or_create(
-        #     fund = fundo
-        # )
     return render(request, 'scraping/start.html')
