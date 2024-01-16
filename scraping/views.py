968c310c65a3b4a7c2feac99e6d02b058fb2d350
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-16 15:09:21 -0300

    :sparkles: feat: Gerando dados no fundo no grafico

--- scraping/views.py
+++ scraping/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import FundsHorus
 from .spider_fii import colect_fund
 
 def start(request):
-    print(colect_fund('VRTA11'))
     if request.method == 'POST':
-        fundo = request.POST.get('fundo')
+        fund_name = request.POST.get('fundo')
+
+        if fund_name:
+            fund_value = colect_fund(fund_name)
+            context = {
+                'fund_value':fund_value,
+                'fund_name':fund_name
+            }
+            return render(request, 'scraping/start.html', context)
+        else:
+            return redirect(request, 'start-fiis')
+
     return render(request, 'scraping/start.html')
